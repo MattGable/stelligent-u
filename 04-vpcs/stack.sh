e2120dccddb6e389defd321f2e47788ec2361175
Matthew Gable <matthew.gable@MACUSSTG2524595.local>
2022-06-23 10:07:34 -0400

    4.1.2

--- 04-vpcs/stack.sh
+++ 04-vpcs/stack.sh
@@ -1,5 +1,5 @@
 #Bucket
-#aws cloudformation validate-template --template-body file://vpc.yaml --template-file file://vpc-params.json
+#aws cloudformation validate-template --template-body file://vpc.yaml
 
 aws cloudformation create-stack --stack-name mattgvpcstack --template-body file://vpc.yaml  --parameters file://vpc-params.json
 
